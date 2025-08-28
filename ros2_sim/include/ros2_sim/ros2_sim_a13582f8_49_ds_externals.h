35b06a0033900b53ecad86bbe3e58431691eb3f7
Andri Wild <andri.wild@fhnw.ch>
2025-08-28 11:40:15 +0000

    remove ros2 matlab exported node

--- ros2_sim/include/ros2_sim/ros2_sim_a13582f8_49_ds_externals.h
+++
@@ -1,4 +0,0 @@
-// Simscape target specific file.
-//  This file is generated for the Simscape network associated with the solver block 'ros2_sim/Solver Configuration1'.
-
-#include "external_std.h"
